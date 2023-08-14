3bd4c303ef92000a6e0560d8fd84685627265c5c
Andrew Hicks <ahicks29@jh.edu>
2023-08-13 21:38:09 -0400

    updated make_project.sh and reduced ip_repo size

--- make_project.sh
+++ make_project.sh
@@ -3,7 +3,9 @@ vivado -nolog -nojournal -mode batch -source ./tcl/make_project.tcl
 # this next command runs the TCL script that makes the bitfile in vivado and exports the XSA
 vivado -nolog -nojournal -mode batch -source ./tcl/impl.tcl
 # this next line makes the .bit.bin file from the .bit file
-bootgen -image doug.bif -arch zynq -process_bitstream bin -w on
+cd tcl
+bootgen -image makebin.bif -arch zynq -process_bitstream bin -w on
+cd ..
 
 # the next line makes the vitis workspace and builds the app thru boot.bin stage
 # commenting out the Vitis section, since we will test this periph in Linux!
